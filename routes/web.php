63136a0887bdf42d66b4124d27fcd07509ae4421
KhaledMohamed08 <khaledmohamed0896@gmail.com>
2025-05-04 15:01:23 +0300

    Install telescope and bootstrab5.3

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/test', function () {
     return view('welcome');
 });
+Route::get('/', function () {
+    return view('app');
+})->name('home');
